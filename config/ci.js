8b1dcd2390dab836f3db950cfeb233ccdc90e70d
khmy2010 <kehan.see@gmail.com>
2018-04-01 17:00:36 +0800

    encrypted travis config

--- config/ci.js
+++ config/ci.js
@@ -1,8 +1,13 @@
 module.exports = {
-    googleClientID:
-        '381928868316-2spb3hkum1o845hv1mbc36cpju05pv5o.apps.googleusercontent.com',
-    googleClientSecret: 'Znwn_rxhuGi0Dox58NVyZPl9',
-    mongoURI: 'mongodb://localhost:27017/Blogster-CI',
-    cookieKey: '123123',
-    redisUrl: 'redis://127.0.0.1:6379'
+    googleClientID: process.env.googleClientID,
+    googleClientSecret: process.env.googleClientSecret,
+    mongoURI: process.env.mongoURI,
+    cookieKey: process.env.cookieKey,
+    redisUrl: process.env.redisUrl
 };
+
+//travis encrypt googleClientID="381928868316-2spb3hkum1o845hv1mbc36cpju05pv5o.apps.googleusercontent.com" --add
+//travis encrypt googleClientSecret="Znwn_rxhuGi0Dox58NVyZPl9" --add
+//travis encrypt mongoURI="mongodb://localhost:27017/Blogster-CI" --add
+//travis encrypt cookieKey="123123" --add
+//travis encrypt redisUrl="redis://127.0.0.1:6379" --add
