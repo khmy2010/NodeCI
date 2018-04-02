3a600d81d2615f008799793f2fc67899b83a33d0
khmy2010 <kehan.see@gmail.com>
2018-04-02 16:10:15 +0800

    with redis

--- services/cache.js
+++ services/cache.js
@@ -36,25 +36,25 @@ mongoose.Query.prototype.exec = async function() {
         return exec.apply(this, arguments);
     }
 
-    // const key = JSON.stringify(
-    //     Object.assign({}, this.getQuery(), {
-    //         collection: this.mongooseCollection.name
-    //     })
-    // );
+    const key = JSON.stringify(
+        Object.assign({}, this.getQuery(), {
+            collection: this.mongooseCollection.name
+        })
+    );
 
-    // const cacheValue = await client.hget(this.hashKey, key);
+    const cacheValue = await client.hget(this.hashKey, key);
 
-    // if (cacheValue) {
-    //     const doc = JSON.parse(cacheValue);
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue);
 
-    //     return Array.isArray(doc)
-    //         ? doc.map(d => new this.model(d))
-    //         : new this.model(doc);
-    // }
+        return Array.isArray(doc)
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc);
+    }
 
     const result = await exec.apply(this, arguments);
 
-    // client.hset(this.hashKey, key, JSON.stringify(result));
+    client.hset(this.hashKey, key, JSON.stringify(result));
     // client.set('fuck', 'me', redis.print);
 
     return result;
